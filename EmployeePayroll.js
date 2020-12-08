052ccacf5a00b811bc2f4d97651e2a6cc90c0d24
Chetan <Chetan@DESKTOP-9SU09BS>
2020-12-08 21:08:36 +0530

    [Chetan]Add: Add regex to validate Id, Gender and Salary.

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -13,8 +13,14 @@ class EmployeePayroll {
         this.gender = params[3];
         this.startDate = params[4];
     }
-
     // getter and setter mwthods
+    get id() { return this._id }
+    set id(id) {
+        let idRegex = RegExp('^[1-9]{1,}$');
+        if (idRegex.test(id))
+            this._id = id;
+        else throw 'Invalid Id';
+    }
     get name() { return this._name; }
     set name(name) {
         let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
@@ -22,7 +28,27 @@ class EmployeePayroll {
             this._name = name;
         else throw 'Name is incorrect!';
     }
-
+    get salary() { return this._salary }
+    set salary(salary) {
+        let salaryRegex = RegExp('^[1-9]+[0-9]{1,}$');
+        if (salaryRegex.test(salary))
+            this._salary = salary;
+        else throw 'Invalid salary';
+    }
+    get gender() { return this._gender; }
+    set gender(gender) {
+        let genderRegex = RegExp('^[MF]$');
+        if (genderRegex.test(gender))
+            this._gender = gender;
+        else throw 'Invalid gender';
+    }
+    get startDate() { return this._startDate; }
+    set startDate(startDate) {
+        let currentDate = new Date();
+        if (startDate <= currentDate)
+            this._startDate = startDate;
+        else throw 'Invalid date';
+    }
     // Method
     toString() {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
@@ -30,15 +56,41 @@ class EmployeePayroll {
         return "id: " + this.id + " ,name: " + this.name + " ,salary: " + this.salary + " ,gender: " + this.gender + " ,startDate: " + empDate;
     }
 }
-
-let employeePayroll = new EmployeePayroll(1, "Shubham", 30000);
+let employeePayroll = new EmployeePayroll(1, "Shubham", 30000, 'M', new Date());
 console.log(employeePayroll.toString());
 try {
-    employeePayroll.id = 2;
+    employeePayroll.id = -1;
+    console.log(employeePayroll.toString());
+}
+catch (e) {
+    console.error(e);
+}
+try{
     employeePayroll.name = "kunal";
     console.log(employeePayroll.toString());
 }
 catch (e) {
+    console.error(e);
+}
+try{
+    employeePayroll.salary = -45000;
+    console.log(employeePayroll.toString());
+}
+catch(e){
+    console.error(e);
+}
+try {
+    employeePayroll.gender = 'h';
+    console.log(employeePayroll.toString());
+}
+catch(e){
+    console.error(e);
+}
+try{
+    employeePayroll.startDate = new Date();
+    console.log(employeePayroll.toString());
+}
+catch(e){
     console.error(e);
 }
 let newEmployeePayroll = new EmployeePayroll(3, "Aashish", 40000, "M", new Date());
